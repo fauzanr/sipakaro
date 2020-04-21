a0bcfb50bec71bad590e3a0e7023ffeb2fc85e74
Kukuh Ardia <ardiajuniar@gmail.com>
2020-04-21 21:00:17 +0700

    Rapihin Folder

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,5 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'welcome';
+
+$route['login'] = 'auth';
+
+$route['admin/index'] = 'admin';
+$route['user/index'] = 'user';
+$route['officer/index'] = 'officer';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
