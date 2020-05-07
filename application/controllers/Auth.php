a1ea59ccd6a715d4596a9d26d881315421997c5d
fauzanr <fauzanr17@gmail.com>
2020-05-08 01:01:52 +0700

    modified:   application/config/autoload.php modified:   application/controllers/Auth.php modified:   application/controllers/Officer.php modified:   application/controllers/Test.php modified:   application/models/Ahp_model.php deleted:    application/models/Pengisian_ahp_model.php
    - hapus tabel & model pengisian_ahp
    - logout -> hapus semua session
    - pengisian ahp jadi dinamis, ditampung di session dulu, baru dari session -> database
    - localhost/test/sess untuk print session
    - modif minor ahp_model

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -89,9 +89,7 @@ class Auth extends CI_Controller {
 
 	public function logout()
 	{
-		$this->session->unset_userdata('email');
-		$this->session->unset_userdata('role_id');
-		$this->session->unset_userdata('admin_logged_in');
+		$this->session->sess_destroy();
 
 		$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"	>Bisa Logout</div>');
 		redirect('/login');
