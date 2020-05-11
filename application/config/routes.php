b273a45d5464fa6282b30e33c2cf98694ee0df61
fauzanr <fauzanr17@gmail.com>
2020-05-11 21:20:06 +0700

    isi tabel section di sql + minor update

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,9 +7,12 @@ $route['login'] = 'auth';
 
 // ADMIN
 $route['admin/index'] = 'admin';
+
 $route['admin/indikator_ayam'] = 'admin/page_indikator_ayam';
 $route['admin/indikator_ayam/tambah'] = 'admin/page_tambah_indikator_ayam';
 $route['admin/indikator_ayam/edit/(:any)'] = 'admin/page_edit_indikator_ayam/$1';
+
+$route['admin/kriteria'] = 'admin/page_kriteria';
 
 // USER
 $route['user/index'] = 'user';
