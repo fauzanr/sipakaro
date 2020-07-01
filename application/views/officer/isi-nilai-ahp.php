6c02ecc11e9e5eb426e14aa8825c37ce10edd67d
Kukuh Ardia <ardiajuniar@gmail.com>
2020-07-01 10:57:36 +0700

    Fix Counter Form isi AHP Ayam

--- application/views/officer/isi-nilai-ahp.php
+++ application/views/officer/isi-nilai-ahp.php
@@ -197,26 +197,26 @@
                 <?php endfor ?>
 
             <!-- looping -->
-                                        <table class="table">
-                                            <thead class="thead-light">
-                                                <tr>
-                                                <th scope="col"></th>
-                                                <th scope="col">Indikator</th>
-                                                <th scope="col">Keterangan</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php $counter = 1 ?>
-                                                <?php foreach($indikator as $j) :?>
-                                                <tr>
-                                                <th scope="row"><?= $counter ?></th>
-                                                <td><?= $j['kode_a_i'] ?></td>
-                                                <td><?= $j['ket_a_i'] ?></td>
-                                                </tr>
-                                                <?php $counter++ ?>
-                                                <?php endforeach ?>
-                                            </tbody>
-                                        </table>                                        
+                <table class="table">
+                    <thead class="thead-light">
+                        <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Indikator</th>
+                        <th scope="col">Keterangan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $counters = 1 ?>
+                        <?php foreach($indikator as $j) :?>
+                        <tr>
+                            <th scope="row"><?= $counters ?></th>
+                                <td><?= $j['kode_a_i'] ?></td>
+                                <td><?= $j['ket_a_i'] ?></td>
+                            </tr>
+                        <?php $counters++ ?>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>                                        
             <?php endif ?>
             
         </div>
